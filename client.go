8d7ecdfbba772023648389854f45ac499ed4980b
eugene <eugene@linux.com>
2021-11-14 15:21:37 +0200

    initial commit

---
+++ client.go
@@ -0,0 +1,140 @@
+package gnosis
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gelfand/gnosis-go/types"
+)
+
+const (
+	BaseURL = "https://protocol-mainnet.gnosis.io/api/v1/"
+)
+
+var errInvalidOrderKind = errors.New("invalid order kind")
+
+var DefaultClient = NewClient("")
+
+type Client struct {
+	url    string
+	client *http.Client
+}
+
+func NewClient(url string) *Client {
+	if url == "" {
+		url = BaseURL
+	}
+
+	return &Client{
+		url: url,
+		client: &http.Client{
+			Timeout: 60 * time.Second,
+		},
+	}
+}
+
+func (c *Client) Fee(ctx context.Context, params types.FeeParams) (types.FeeResponse, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", c.url+"fee", nil)
+	if err != nil {
+		return types.FeeResponse{}, err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	q := req.URL.Query()
+	q.Add("sellToken", params.SellToken)
+	q.Add("buyToken", params.BuyToken)
+	q.Add("amount", params.Amount)
+	q.Add("kind", params.Kind)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return types.FeeResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var res types.FeeResponse
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		return types.FeeResponse{}, err
+	}
+
+	return res, nil
+}
+
+func (c *Client) CreateOrder(ctx context.Context, params types.OrderParams) (string, error) {
+	body, err := json.Marshal(params)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", c.url+"orders", bytes.NewReader(body))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var res string
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
+
+func (c *Client) Orders(ctx context.Context, params types.OrdersParams) ([]types.Order, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", c.url+"orders", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	q := req.URL.Query()
+
+	if params.Owner != "" {
+		q.Add("owner", params.Owner)
+	}
+
+	if params.SellToken != "" {
+		q.Add("sellToken", params.SellToken)
+	}
+
+	if params.BuyToken != "" {
+		q.Add("buyToken", params.BuyToken)
+	}
+	q.Add("includeFullyExecuted", strconv.FormatBool(params.IncludeFullyExecuted))
+	q.Add("includeInvalidated", strconv.FormatBool(params.IncludeInvalidated))
+	q.Add("includeInsufficientBalance", strconv.FormatBool(params.IncludeInsufficientBalance))
+	q.Add("includePresignaturePending", strconv.FormatBool(params.IncludePresignaturePending))
+	q.Add("includeUnsupportedTokens", strconv.FormatBool(params.IncludeUnsupportedTokens))
+	q.Add("minValidTo", strconv.Itoa(int(params.MinValidTo)))
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var res []types.Order
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func Orders(ctx context.Context, params types.OrdersParams) ([]types.Order, error) {
+	return DefaultClient.Orders(ctx, params)
+}
---
+++ client_test.go
@@ -0,0 +1,58 @@
+package gnosis
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/gelfand/gnosis-go/types"
+)
+
+var client = NewClient("")
+
+func TestClient_Fee(t *testing.T) {
+	ctx := context.Background()
+	_, err := client.Fee(ctx, types.FeeParams{
+		SellToken: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+		BuyToken:  "0xdac17f958d2ee523a2206206994597c13d831ec7",
+		Amount:    "100000000",
+		Kind:      "buy",
+	})
+	if err != nil {
+		t.Errorf("Client.Fee() error = %v, wantErr = false", err)
+	}
+}
+
+func TestClient_Orders(t *testing.T) {
+	ctx := context.Background()
+	resp, err := client.Orders(ctx, types.OrdersParams{
+		Owner:                      "0x7BB8536Bac7d6e5c4E352Ce50be8968d5d6cd445",
+		IncludeFullyExecuted:       true,
+		IncludeInvalidated:         true,
+		IncludeInsufficientBalance: true,
+		IncludePresignaturePending: true,
+		IncludeUnsupportedTokens:   true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(resp)
+}
+
+func TestOrders(t *testing.T) {
+	ctx := context.Background()
+	resp, err := Orders(ctx, types.OrdersParams{
+		Owner:                      "0x7BB8536Bac7d6e5c4E352Ce50be8968d5d6cd445",
+		IncludeFullyExecuted:       true,
+		IncludeInvalidated:         true,
+		IncludeInsufficientBalance: true,
+		IncludePresignaturePending: true,
+		IncludeUnsupportedTokens:   true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(resp)
+}
